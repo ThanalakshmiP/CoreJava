a09da4376b743a6e423836375a6d2b4654c63c98
ThanalakshmiP <131784393+ThanalakshmiP@users.noreply.github.com>
2023-10-17 11:54:06 +0530

    Update Basic.java

--- TnsifJava/src/com/dayone/Basic.java
+++ TnsifJava/src/com/dayone/Basic.java
@@ -2,7 +2,7 @@ package com.dayone;
 
 public class Basic {
 	public static void main(String args[]) {
-		System.out.println("Hello, I'm Thanalakshmiiiiiii, \n Simply Wasted Girl");
+		System.out.println("Hello, I'm Thanalakshmi");
 	}
 
 }
